560ab85e67c710c5ba2a07159f139bf271917f1b
Mario Reder <mario.reder@pm.me>
2025-10-06 17:01:34 +0200

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -17,7 +17,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_APP_DESCRIPTION": "Orderly Trading Application",
   "VITE_HAS_PRIMARY_LOGO": "true",
   "VITE_HAS_SECONDARY_LOGO": "false",
-  "VITE_ENABLED_MENUS": "Trading,Markets,Portfolio,Vaults",
+  "VITE_ENABLED_MENUS": "Trading,Markets,Portfolio",
   "VITE_CUSTOM_MENUS": "X, https://x.com/GREENDEXTRADE",
   "VITE_ENABLE_CAMPAIGNS": "false",
   "VITE_TELEGRAM_URL": "",
